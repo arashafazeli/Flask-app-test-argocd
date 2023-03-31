50fde4260310a259a0d3e6864fd41a4fe359db11
Arash Afazeli <arashafazeli@gmail.com>
2023-04-01 00:14:37 +0200

    add github actions workflow to build and push image to gcr

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def msg():
-    return "Hello! Deployed using Argo CD!"
+    return "Hello! Deployed with Argo CD!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
